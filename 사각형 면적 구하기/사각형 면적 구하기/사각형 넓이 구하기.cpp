b6395920203fae9449d1522780fcd6c852b1ebaa
libertar <143467498+chengeanLim@users.noreply.github.com>
2024-02-22 19:42:49 +0900

    Add files via upload

---
+++ 사각형 면적 구하기/사각형 면적 구하기/사각형 넓이 구하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+	int area;
+
+	cout << "가로 길이를 입력하세요 >>";
+	cin >> x;
+	cout << "세로 길이를 입력하세요 >>";
+	cin >> y;
+	area = x * y;
+	cout << "사각형의 면적은" << area << endl;
+
+}
